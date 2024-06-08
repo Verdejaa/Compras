b66cef7064693f87903b8906667eef6e3289f0a1
Verdejaa <jorge.verdeja.2608@gmail.com>
2024-06-08 00:27:21 -0600

    Add files via upload

---
+++ CAMBIOS.js
@@ -0,0 +1,69 @@
+function KIDS(){
+	document.getElementById('INICIO').style.display = 'None';
+	document.getElementById('KIDS').style.display = 'Block';
+	document.getElementById('DAMA').style.display = 'None';
+	document.getElementById('CABALLERO').style.display = 'None';
+	document.getElementById('PERFUMES').style.display = 'None';
+	document.getElementById('ELECTRONICA').style.display = 'None';
+	document.getElementById('MINI').style.display = 'None';
+	document.getElementById('UNISEX').style.display = 'None';}
+
+function DAMA(){
+	document.getElementById('INICIO').style.display = 'None';
+	document.getElementById('KIDS').style.display = 'None';
+	document.getElementById('DAMA').style.display = 'Block';
+	document.getElementById('CABALLERO').style.display = 'None';
+	document.getElementById('PERFUMES').style.display = 'None';
+	document.getElementById('ELECTRONICA').style.display = 'None';
+	document.getElementById('MINI').style.display = 'None';
+	document.getElementById('UNISEX').style.display = 'None';}
+
+function CABALLERO(){
+	document.getElementById('INICIO').style.display = 'None';
+	document.getElementById('KIDS').style.display = 'None';
+	document.getElementById('DAMA').style.display = 'None';
+	document.getElementById('CABALLERO').style.display = 'Block';
+	document.getElementById('PERFUMES').style.display = 'None';
+	document.getElementById('ELECTRONICA').style.display = 'None';
+	document.getElementById('MINI').style.display = 'None';
+	document.getElementById('UNISEX').style.display = 'None';}
+
+function ELECTRONICA(){
+	document.getElementById('INICIO').style.display = 'None';
+	document.getElementById('KIDS').style.display = 'None';
+	document.getElementById('DAMA').style.display = 'None';
+	document.getElementById('CABALLERO').style.display = 'None';
+	document.getElementById('PERFUMES').style.display = 'None';
+	document.getElementById('ELECTRONICA').style.display = 'Block';
+	document.getElementById('MINI').style.display = 'None';
+	document.getElementById('UNISEX').style.display = 'None';}
+
+function PERFUMES(){
+	document.getElementById('INICIO').style.display = 'None';
+	document.getElementById('KIDS').style.display = 'None';
+	document.getElementById('DAMA').style.display = 'None';
+	document.getElementById('CABALLERO').style.display = 'None';
+	document.getElementById('PERFUMES').style.display = 'Block';
+	document.getElementById('ELECTRONICA').style.display = 'None';
+	document.getElementById('MINI').style.display = 'None';
+	document.getElementById('UNISEX').style.display = 'None';}
+
+function UNISEX(){
+	document.getElementById('INICIO').style.display = 'None';
+	document.getElementById('KIDS').style.display = 'None';
+	document.getElementById('DAMA').style.display = 'None';
+	document.getElementById('CABALLERO').style.display = 'None';
+	document.getElementById('PERFUMES').style.display = 'None';
+	document.getElementById('ELECTRONICA').style.display = 'None';
+	document.getElementById('MINI').style.display = 'None';
+	document.getElementById('UNISEX').style.display = 'Block';}
+
+function MINI(){
+	document.getElementById('INICIO').style.display = 'None';
+	document.getElementById('KIDS').style.display = 'None';
+	document.getElementById('DAMA').style.display = 'None';
+	document.getElementById('CABALLERO').style.display = 'None';
+	document.getElementById('PERFUMES').style.display = 'None';
+	document.getElementById('ELECTRONICA').style.display = 'None';
+	document.getElementById('MINI').style.display = 'Block';
+	document.getElementById('UNISEX').style.display = 'None';}
